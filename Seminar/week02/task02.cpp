dd7b65df75cf4f2da284acaaf7e72f186d48e42c
AleksManolow <amanolov7@gmail.com>
2022-10-16 15:31:51 +0300

    Add task02-Brackets

---
+++ Seminar/week02/task02.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stack>
+#include<string>
+bool isStringBalanced(const std::string& str)
+{
+    std::stack<char> s;
+
+    for (size_t i = 0; i < str.size(); i++)
+    {
+        if(str[i] == ')'){
+            if(s.empty())
+                return false;
+            s.pop();
+        }
+        else {
+            s.push(str[i]);
+        }
+    }
+    return s.empty();
+}
+int main()
+{
+    std::cout << isStringBalanced("((()))") << std::endl;
+    std::cout << isStringBalanced("))()") << std::endl;
+    std::cout << isStringBalanced(")(") << std::endl;
+    std::cout << isStringBalanced("()()(())") << std::endl;
+
+    return 0;
+}
