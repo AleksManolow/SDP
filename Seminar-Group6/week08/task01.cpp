ed666d448e345624a60bff2e42a6917301eb92b7
AleksManolow <amanolov7@gmail.com>
2022-12-31 16:22:59 +0200

    Add week08

--- Seminar-Group6/week08/task01.cpp
+++ Seminar-Group6/week08/task01.cpp
@@ -7,6 +7,12 @@ struct Node
 
     Node(int _key):key(_key), left(nullptr), right(nullptr){}
 };
+void clean(Node*& n) {
+    if (n == nullptr) return;
+    clean(n->left);
+    clean(n->right);
+    delete n;
+}
 Node* insert(Node* tree, int el)
 {
     if (!tree)
@@ -14,19 +20,12 @@ Node* insert(Node* tree, int el)
         Node* newNode = new Node(el);
         return newNode;
     }
-
     if (el < tree->key)
         tree->left = insert(tree->left, el);
     else
         tree->right = insert(tree->right, el);
     
     return tree;
-}
-void clean(Node*& n) {
-    if (n == nullptr) return;
-    clean(n->left);
-    clean(n->right);
-    delete n;
 }
 int main()
 {
@@ -35,11 +34,10 @@ int main()
     test->right = new Node(140);
     test->left->left = new Node(25);
     test = insert(test, 150);
-    std::cout << (test->right->right->key == 150) << std::endl;
+    std::cout << ((test->right->right->key == 150) ? "True" : "False") << std::endl;
     test = insert(test, 75);
-    std::cout << (test->left->right->key == 75) << std::endl;
+    std::cout << ((test->left->right->key == 75) ? "True" : "False")<< std::endl;
 
-    clean(test);    
-
+    clean(test);
     return 0;
 }
--- Seminar-Group6/week08/task02.cpp
+++ Seminar-Group6/week08/task02.cpp
@@ -7,21 +7,21 @@ struct Node
 
     Node(int _key):key(_key), left(nullptr), right(nullptr){}
 };
-bool isContains(Node* tree, int value)
-{
-    if(!tree)
-        return false;
-    if(tree->key == value)
-        return true;
-    if(tree->key > value)
-        return isContains(tree->left, value);
-    return isContains(tree->right, value);
-}
 void clean(Node*& n) {
     if (n == nullptr) return;
     clean(n->left);
     clean(n->right);
     delete n;
+}
+bool contains(Node* tree, int value)
+{
+    if (!tree)
+        return false;
+    if (tree->key == value)
+        return true;
+    if (tree->key > value)
+        return contains(tree->left, value);
+    return contains(tree->right, value);
 }
 int main()
 {
@@ -30,11 +30,10 @@ int main()
     test->right = new Node(140);
     test->left->left = new Node(25);
 
-    std::cout << (isContains(test, 140) == true) << std::endl;
-    std::cout << (isContains(test, 25) == true) << std::endl;
-    std::cout << (isContains(test, 0) == false) << std::endl;
-    std::cout << (isContains(test, 150) == false) << std::endl;
-    clean(test); 
-
+    std::cout << ((contains(test, 140) == true) ? "True" : "False") << std::endl;
+    std::cout << ((contains(test, 25) == true) ? "True" : "False") << std::endl;
+    std::cout << ((contains(test, 0) == false) ? "True" : "False") << std::endl;
+    std::cout << ((contains(test, 150) == false) ? "True" : "False") << std::endl;
+    clean(test);
     return 0;
 }
